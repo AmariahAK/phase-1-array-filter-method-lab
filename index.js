5af2c0e3b9620285b54de14a852ec172fdc2a74a
Amariah <amariah.abish@gmail.com>
2024-03-22 09:39:02 +0300

    P1 Array Filter Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+// Function to find matching drivers (case insensitive)
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // Function to find drivers whose names begin with the provided letters
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+  
+  // Function to match driver names with the provided string
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name.toLowerCase() === query.toLowerCase());
+  }
+  
